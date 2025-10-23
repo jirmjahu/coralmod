53c4d82488deefe18f60368f468c21fb76ffe515
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-10-23 15:24:18 +0200

    Migrate project to Kotlin Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id ("java")
+	alias(libs.plugins.loom)
+}
+
+version = "0.0.1"
+group = "net.coralmod.mod"
+
+repositories {
+
+}
+
+dependencies {
+    minecraft(libs.minecraft)
+    mappings(libs.mappings)
+    modImplementation(libs.loader)
+    modImplementation(libs.api)
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}

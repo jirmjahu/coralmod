1401f5f6e0861f558a173d5ff57f50f9214f67af
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-10-24 15:58:10 +0200

    Added fist render functionality to HudModule

--- src/main/java/net/coralmod/mod/module/HudModule.java
+++ src/main/java/net/coralmod/mod/module/HudModule.java
@@ -3,8 +3,11 @@ package net.coralmod.mod.module;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import net.coralmod.mod.module.settings.BooleanSetting;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+
+import java.awt.*;
 
 @Getter
 @Setter
@@ -15,13 +18,49 @@ public abstract class HudModule extends Module {
     private int y;
     private int width;
     private int height;
-    private String message;
+    private String text;
+
+    protected BooleanSetting background = new BooleanSetting("Background", true);
+    protected BooleanSetting brackets = new BooleanSetting("Brackets", false);
+    protected BooleanSetting textShadow = new BooleanSetting("Text Shadow", true);
 
     public HudModule(int x, int y) {
         this.x = x;
         this.y = y;
+        addSettings(background, brackets, textShadow);
     }
 
     public void render(DrawContext context, TextRenderer textRenderer) {
+        final String text = brackets.getValue() ? "[" + this.text + "]" : this.text;
+
+        final int textWidth = textRenderer.getWidth(text);
+        final int textHeight = textRenderer.fontHeight;
+
+        final int padding = background.getValue() ? 4 : 0;
+
+        width = textWidth + padding * 2;
+        height = textHeight + padding * 2;
+
+        if (background.getValue()) {
+            context.fill(
+                    x,
+                    y,
+                    x + width,
+                    y + height,
+                    new Color(0, 0, 0, 140).getRGB()
+            );
+        }
+
+        final int textX = x + (width - textWidth) / 2;
+        final int textY = y + (height - textHeight) / 2 + 1;
+
+        context.drawText(
+                textRenderer,
+                text,
+                textX,
+                textY,
+                -1,
+                textShadow.getValue()
+        );
     }
 }
